abbc607b24d93718f6504374f4f13c87546aa7e4
Matteo Medda <m.medda@doveconviene.com>
2020-09-25 18:17:11 +0200

    Updates

---
+++ precache-manifest.88cc978c87ecdb6b7a723396a5a9614d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dad10e765a6c5827e33f2e0aa12bdd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e98a6fd4fedaa038d9b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dd4ca39354d5f981bc4e",
+    "url": "/static/js/2.1b4c373c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1b4c373c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e98a6fd4fedaa038d9b",
+    "url": "/static/js/main.f7e0bdd7.chunk.js"
+  },
+  {
+    "revision": "d1034df4a16803a98f6a",
+    "url": "/static/js/runtime-main.e2a8760c.js"
+  }
+]);
