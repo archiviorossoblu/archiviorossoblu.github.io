acdacffd34b3f976e0f98acdc4bbc96cefd9c5ec
Matteo Medda <m.medda@doveconviene.com>
2020-09-30 21:41:41 +0200

    Updates

--- precache-manifest.08c3dfb37be6a526601ded539bc41b3e.js
+++ precache-manifest.08c3dfb37be6a526601ded539bc41b3e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58df0f3dc6a283e0f09584f1f30e3870",
+    "revision": "efe362ee30b4f4153c15645206366fbc",
     "url": "/index.html"
   },
   {
-    "revision": "171975c5f6978cab016e",
+    "revision": "0485a96e216826ce1793",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "e371d5a40f444fa83edf",
-    "url": "/static/js/2.d105b872.chunk.js"
+    "revision": "337873000e372b7c3096",
+    "url": "/static/js/2.19ed8557.chunk.js"
   },
   {
     "revision": "f15f3d5e75ed75f40e5d72cacec0985a",
-    "url": "/static/js/2.d105b872.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.19ed8557.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "171975c5f6978cab016e",
-    "url": "/static/js/main.8b41ff75.chunk.js"
+    "revision": "0485a96e216826ce1793",
+    "url": "/static/js/main.7182d9a8.chunk.js"
   },
   {
     "revision": "4cd1221fbb0987f52117",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e4622eccbdaa10dbaf486e8b8f1cdac.js"
+  "/precache-manifest.08c3dfb37be6a526601ded539bc41b3e.js"
 );
 
 self.addEventListener('message', (event) => {
